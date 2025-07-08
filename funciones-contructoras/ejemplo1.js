7718be1750be31795262883b0f53050bcd522039
Grecia Delgado <greciadm2596@gmail.com>
2025-07-07 18:24:25 -0600

    feat:create animal object

--- funciones-contructoras/ejemplo1.js
+++ funciones-contructoras/ejemplo1.js
@@ -1,1 +1,21 @@
 // aqui van ejemplos
+let animal = {}; // este es un objeto vacio
+
+animal.nombre = "gato";
+animal.especie = "felino";
+animal.energia = 10;
+
+animal.comer = function (cantidad) {
+  console.log(`${this.nombre} esta comiendo...`);
+  this.energia += cantidad;
+};
+
+animal.dormir = function (tiempo) {
+  console.log(`${this.nombre} está durmiendo...`);
+  this.energia += tiempo;
+};
+
+animal.jugar = function (tiempo) {
+  console.log(`${this.nombre} esta jugando...`);
+  this.energia -= tiempo;
+};
