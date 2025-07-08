a47a8caa8b25fa4b4aead0aefcf6ac69bf8b2579
Grecia Delgado <greciadm2596@gmail.com>
2025-07-07 18:58:34 -0600

    add ejemplo 2

--- funciones-contructoras/ejemplo2.js
+++ funciones-contructoras/ejemplo2.js
@@ -2,6 +2,22 @@
 
 // tengo que encontrar la forma mas efectiva de crearlos , para que NO repitamos codigo
 
+// Instanciación Funcional con Métodos Compartidos
+const funciones_vitales = {
+  comer(cantidad) {
+    console.log(`${this.nombre} esta comiendo...`);
+    this.energia += cantidad;
+  },
+  dormir(tiempo) {
+    console.log(`${this.nombre} está durmiendo...`);
+    this.energia += tiempo;
+  },
+  jugar(tiempo) {
+    console.log(`${this.nombre} esta jugando...`);
+    this.energia -= tiempo;
+  },
+};
+
 // quiero que esta funcion "Animal"
 // sea la encargada de cerar nuevos animales
 function Animal(nombre, energia) {
@@ -10,27 +26,16 @@ function Animal(nombre, energia) {
   animal.nombre = nombre;
   animal.energia = energia;
 
-  animal.comer = function (cantidad) {
-    console.log(`${this.nombre} esta comiendo...`);
-    this.energia += cantidad;
-  };
-
-  animal.dormir = function (tiempo) {
-    console.log(`${this.nombre} está durmiendo...`);
-    this.energia += tiempo;
-  };
-
-  animal.jugar = function (tiempo) {
-    console.log(`${this.nombre} esta jugando...`);
-    this.energia -= tiempo;
-  };
+  animal.comer = funciones_vitales.comer;
+  animal.dormir = funciones_vitales.dormir;
+  animal.jugar = funciones_vitales.jugar;
 
   return animal;
 }
 
-const animal_1 = Animal("perro", 100);
-const animal_2 = Animal("tortuga", 50);
-const animal_3 = Animal("panda", 80);
-console.log(animal_1)
-console.log(animal_2)
-console.log(animal_3)
+const animal_1 = Animal("perro", 100); // nueva instancia
+const animal_2 = Animal("tortuga", 50); // nueva instancia
+const animal_3 = Animal("panda", 80); // nueva instancia
+console.log(animal_1);
+console.log(animal_2);
+console.log(animal_3);
