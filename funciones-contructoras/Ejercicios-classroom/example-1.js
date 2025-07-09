fa1ecf47ba28c55feac5b48c64432f62d781e173
Grecia Delgado <greciadm2596@gmail.com>
2025-07-08 19:17:37 -0600

    Add exmaple

--- funciones-contructoras/Ejercicios-classroom/example-1.js
+++ funciones-contructoras/Ejercicios-classroom/example-1.js
@@ -1,8 +1,6 @@
 // PASO 1 : Crear funcion Constructora
 function Contador(valor) {
-  let contador = { valor: valor || 0 };
-
-  return contador;
+  this.contador = valor || 0;
 }
 
 // PASO 2:  Extender el prototipo de Contador
@@ -25,15 +23,40 @@ Contador.prototype.mostrarValor = function () {
   return this.contador;
 };
 
+Contador.prototype.multiplicarX2 = function () {
+  return this.contador * 2;
+};
+
+const listofFunciones = {
+  incrementar: () => {
+    return (this.contador += 1);
+  },
+  mostrarValor: () => {
+    return this.contador;
+  },
+};
+
 const miContador = new Contador();
 
 // (**) punto de control para el diagrama VV
+//console.log("que es mi contador ?????", miContador);
+//console.log("que es mi contador Type of ?????", typeof miContador);
+
+// miContador.__proto__ = listofFunciones;
 
 miContador.incrementar();
 miContador.incrementar();
 miContador.incrementar();
-console.log(miContador.mostrarValor()); // Debería imprimir: 3
+console.log(miContador.mostrarValor());
 
+console.log("---------DATOS DE LA INSTANCIA -------");
+console.log("Mi Contador solo: ", miContador);
+console.log("Mi contador __proto__: ", miContador.__proto__);
+console.log("que hay dentro??", Object.getPrototypeOf(miContador));
+console.log("Mi contador , tiene prototype ????", miContador.prototype)
+
+console.log("---------DATOS DE LA FUNCION CONSTRUCTORA -------");
+console.log("Contador prototype ???: ", Contador.prototype);
 // miContador.decrementar();
 // console.log(miContador.mostrarValor()); // Debería imprimir: 2
 
